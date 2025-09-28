c19263b579ca9415d049104f19c6a7afc426067e
leonardosecchin <secchinleo@gmail.com>
2025-09-27 23:20:06 -0300

    Adding --cuda flag in run_allthreads.sh

--- benchmark/run_allthreads.sh
+++ benchmark/run_allthreads.sh
@@ -3,5 +3,5 @@
 rm -f results.jld2
 
 for t in 1 2 4 8 16 24 48; do
-    nice -n 0 julia -t $t benchmark.jl --continue true --nreps 20
+    nice -n 0 julia -t $t benchmark.jl --continue true --nreps 20 --cuda 32
 done
