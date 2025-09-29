a2e189af1f99ca5f4ca5f95be3eee19cceaaf98c
Paulo J S Silva <pjssilva@gmail.com>
2025-09-29 08:47:08 -0300

    Use the new --cuda option in the test script

--- benchmark/run_allthreads.sh
+++ benchmark/run_allthreads.sh
@@ -3,5 +3,5 @@
 rm -f results.jld2
 
 for t in 1 2 4 8 16 24 48; do
-    nice -n 0 julia -t $t benchmark.jl --continue true --nreps 20
+    nice -n 0 julia-1.10 --project -t $t benchmark.jl --cuda 64 --continue true --nreps 20
 done
